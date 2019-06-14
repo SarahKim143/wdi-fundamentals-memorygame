4f0dbcd6fad729446ce29d56946b7559fad0e399
Sarah Kim <sarahckim143@gmail.com>
2019-06-14 16:52:36 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,27 @@
 console.log("Up and running!");
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/images/king-of-diamonds.png",
+	},
+	{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/images/king-of-hearts.png",
+	},
+	{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/images/queen-of-diamonds.png",	
+	},
+	{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/images/queen-of-hearts.png",	
+	}
+];
+
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -20,8 +42,11 @@ if (cardsInPlay.length === 2) {
 	alert("Sorry, try again.");
 	} 	
 
-console.log("User flipped" + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped" + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 }
+
 	flipCard(0);
 	flipCard(2);
