729d40004e042c6d13089be35d4b7248755f74f7
Sarah Kim <sarahckim143@gmail.com>
2019-06-14 18:23:55 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -32,21 +32,29 @@ function checkForMatch() {
 }
 }	
 
-function flipCard(cardId) {
-	checkForMatch();
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	console.log("User flipped " + cards[cardId].rank + "of " + cards[cardId].suit);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]);
 	alert("You found a match!");
-}
-	else {
-	alert("Sorry, try again.");
-	} 	
+	}
+}	
+checkForMatch();	
 
-console.log("User flipped" + cards[cardId].rank);
-cardsInPlay.push(cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
-}
 
-	flipCard(0);
-	flipCard(2);
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+ var cardElement = document.createElement('img');
+ cardElement.setAttribute('src', 'images/images/back.png');
+ cardElement.setAttribute('data-id', i);
+ cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+}
+}
+createBoard();
